8b947900d54f36170e3dd6476b9c973c7a64e63b
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 14:46:30 +0100

    edit js

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,7 @@
+export default function AppFooter() {
+    return (
+        <footer>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus nesciunt quo ipsum quos neque placeat aspernatur aut magni tenetur eos excepturi vero repellendus consectetur vitae, accusantium fugiat, at cupiditate aliquam Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rem ea asperiores voluptate maxime fugit non ex necessitatibus, debitis ab sint suscipit, minus magnam, cumque in? Aspernatur recusandae harum obcaecati!</p>
+        </footer>
+    )
+}
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+export default function AppHeader() {
+    return (
+        <header>
+            <div className="container">
+                <h1>il mio blog</h1>
+            </div>
+        </header>
+    )
+}
