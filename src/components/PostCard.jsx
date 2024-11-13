31acb8bca43a853e8f2aae8486be4a55f000c82b
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 17:16:39 +0100

    edit js

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,6 +1,16 @@
-<div className="card">
-    <img src="https://www.galatina.it/sites/default/files/articles/Il%20sommergibile%20Pietro%20Venuti%20%28foto%20Marina%20Militare%29-2.jpg" alt="" />
-    <h2 className="card-title">titolo del post</h2>
-    <p className="card-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis pariatur quaerat, atque assumenda, aperiam maiores voluptatum provident amet, dignissimos accusantium culpa delectus. Perspiciatis commodi impedit, fugit quasi quis tenetur vero!</p>
-    <a className="btn" href="">LEGGI DI PIù</a>
-</div>
+export default function postsCard({ post }) {
+
+
+
+    return (
+        <div className="card">
+            <img src={post.image} alt="" />
+            <div className="card-body">
+                <h3>{post.name}</h3>
+                <div className="tags"> tags:html ,css </div>
+                <div className="title"> title:cagnolino</div>
+
+            </div>
+        </div>
+    )
+}
