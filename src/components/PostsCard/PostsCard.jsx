0400b06b121e61711243f398d820260adc02e23b
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 16:56:20 +0100

    edit js

---
+++ src/components/PostsCard/PostsCard.jsx
@@ -0,0 +1,13 @@
+export default function postsCard() {
+    return (
+        <div className="card">
+            <img src="public/image/237-536x354.jpg" alt="" />
+            <div className="card-body">
+                <h3>cagnolino</h3>
+                <div className="tags"> tags:html ,css </div>
+                <div className="title"> title:cagnolino</div>
+
+            </div>
+        </div>
+    )
+}
