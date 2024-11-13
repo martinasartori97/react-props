860720535eaa7b4fe05453048b8528f18bab775f
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 16:04:46 +0100

    edit js

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,13 +1,9 @@
+import posts from "./data/posts"
 export default function AppMain() {
     return (
         <main>
             <div className="container">
-                <div className="card">
-                    <img src="https://www.galatina.it/sites/default/files/articles/Il%20sommergibile%20Pietro%20Venuti%20%28foto%20Marina%20Militare%29-2.jpg" alt="" />
-                    <h2 className="card-title">titolo del post</h2>
-                    <p className="card-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis pariatur quaerat, atque assumenda, aperiam maiores voluptatum provident amet, dignissimos accusantium culpa delectus. Perspiciatis commodi impedit, fugit quasi quis tenetur vero!</p>
-                    <a className="btn" href="">LEGGI DI PIù</a>
-                </div>
+
             </div>
         </main>
     )
