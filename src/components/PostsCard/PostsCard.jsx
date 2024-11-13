1615ee9e06b48b39927be6c0fdb36888439e5d6f
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 17:02:25 +0100

    edit js

--- src/components/PostsCard/PostsCard.jsx
+++ src/components/PostsCard/PostsCard.jsx
@@ -1,9 +1,12 @@
-export default function postsCard() {
+export default function postsCard({ post }) {
+
+
+
     return (
         <div className="card">
-            <img src="public/image/237-536x354.jpg" alt="" />
+            <img src={post.image} alt="" />
             <div className="card-body">
-                <h3>cagnolino</h3>
+                <h3>{post.name}</h3>
                 <div className="tags"> tags:html ,css </div>
                 <div className="title"> title:cagnolino</div>
 
