e1761d06b4ae5ec552c62bb25590b3b31c679dc7
martinasartori97 <martinasartori9@gmail.com>
2024-11-13 17:56:26 +0100

    edit js

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,17 +1,13 @@
-import posts from "./data/posts"
+import Posts from "./data/posts.js"
+import postsCard from "./PostCard.jsx"
+
+
 export default function AppMain() {
     return (
         <main>
             <div className="container">
-                <div className="card">
-                    <img src="public/image/237-536x354.jpg" alt="" />
-                    <div className="card-body">
-                        <h3>cagnolino</h3>
-                        <div className="tags"> tags:html ,css </div>
-                        <div className="title"> title:cagnolino</div>
 
-                    </div>
-                </div>
+                {Posts.map(post => <PostsCard {Posts} key={post.id} />)}
 
             </div>
         </main>
