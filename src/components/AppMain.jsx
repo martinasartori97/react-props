6d209f3efb26658268e6aabc6802c734c191092f
martinasartori97 <martinasartori9@gmail.com>
2024-11-14 21:28:28 +0100

    edit js

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,12 +2,17 @@ import Posts from "./data/posts.js"
 import PostCard from "./PostCard.jsx"
 
 
+
 export default function AppMain() {
+
+
+
     return (
         <main>
             <div className="container">
 
                 {Posts.map(post => (<PostCard post={Posts} key={post.id} />))}
+
             </div>
         </main>
     )
